a1deac1a580162d951bd7ad26fd4b393894abdb5
Daniel Song <57929778+dsong831@users.noreply.github.com>
2024-10-15 21:39:21 +0900

    arm_gpio.c 업데이트

--- C_Rework/ai_driver/arm_gpio.c
+++ C_Rework/ai_driver/arm_gpio.c
@@ -1,129 +1,84 @@
 #include "gpio.h"
 
-#define GPIO_PIN_MASK 0x000000FF
+#define GPIO_PIN_MASK(pin) (1UL << (pin))
 
 void GPIO_Init(GPIO_TypeDef *GPIOx, GPIO_Config *config) {
-    uint32_t pin_mask = 1 << config->pin;
+    uint32_t pin_mask = GPIO_PIN_MASK(config->pin);
 
     // Set direction
     if (config->is_output) {
-        GPIOx->DIR |= pin_mask;
+        GPIOx->OUTENSET = pin_mask;
     } else {
-        GPIOx->DIR &= ~pin_mask;
+        GPIOx->OUTENCLR = pin_mask;
     }
 
-    // Configure pull-up/pull-down
-    if (config->pull_up) {
-        GPIOx->PUR |= pin_mask;
+    // Configure alternate function
+    if (config->use_alt_func) {
+        GPIOx->ALTFUNCSET = pin_mask;
     } else {
-        GPIOx->PUR &= ~pin_mask;
-    }
-    if (config->pull_down) {
-        GPIOx->PDR |= pin_mask;
-    } else {
-        GPIOx->PDR &= ~pin_mask;
+        GPIOx->ALTFUNCCLR = pin_mask;
     }
 
-    // Configure open drain
-    if (config->open_drain) {
-        GPIOx->ODR |= pin_mask;
+    // Configure interrupt
+    if (config->enable_interrupt) {
+        GPIOx->INTENSET = pin_mask;
+        if (config->interrupt_type_edge) {
+            GPIOx->INTTYPESET = pin_mask;
+        } else {
+            GPIOx->INTTYPECLR = pin_mask;
+        }
+        if (config->interrupt_pol_high) {
+            GPIOx->INTPOLSET = pin_mask;
+        } else {
+            GPIOx->INTPOLCLR = pin_mask;
+        }
     } else {
-        GPIOx->ODR &= ~pin_mask;
-    }
-
-    // Configure drive strength
-    switch (config->drive_strength) {
-        case 2:
-            GPIOx->DR2R |= pin_mask;
-            GPIOx->DR4R &= ~pin_mask;
-            GPIOx->DR8R &= ~pin_mask;
-            break;
-        case 4:
-            GPIOx->DR2R &= ~pin_mask;
-            GPIOx->DR4R |= pin_mask;
-            GPIOx->DR8R &= ~pin_mask;
-            break;
-        case 8:
-            GPIOx->DR2R &= ~pin_mask;
-            GPIOx->DR4R &= ~pin_mask;
-            GPIOx->DR8R |= pin_mask;
-            break;
-        default:
-            // Default to 2mA if invalid value is provided
-            GPIOx->DR2R |= pin_mask;
-            GPIOx->DR4R &= ~pin_mask;
-            GPIOx->DR8R &= ~pin_mask;
-    }
-
-    // Configure analog mode
-    if (config->is_analog) {
-        GPIOx->AMSEL |= pin_mask;
-        GPIOx->DEN &= ~pin_mask;
-    } else {
-        GPIOx->AMSEL &= ~pin_mask;
-        GPIOx->DEN |= pin_mask;
+        GPIOx->INTENCLR = pin_mask;
     }
 }
 
 void GPIO_WritePin(GPIO_TypeDef *GPIOx, uint8_t pin, bool state) {
-    GPIOx->DATA[1 << pin] = state ? (1 << pin) : 0;
+    uint32_t pin_mask = GPIO_PIN_MASK(pin);
+    if (state) {
+        GPIOx->DATAOUT |= pin_mask;
+    } else {
+        GPIOx->DATAOUT &= ~pin_mask;
+    }
 }
 
 bool GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint8_t pin) {
-    return (GPIOx->DATA[1 << pin] != 0);
+    return (GPIOx->DATA & GPIO_PIN_MASK(pin)) != 0;
 }
 
 void GPIO_TogglePin(GPIO_TypeDef *GPIOx, uint8_t pin) {
-    GPIOx->DATA[1 << pin] ^= (1 << pin);
+    GPIOx->DATAOUT ^= GPIO_PIN_MASK(pin);
 }
 
-void GPIO_EnableInterrupt(GPIO_TypeDef *GPIOx, uint8_t pin, GPIO_TriggerType trigger) {
-    uint32_t pin_mask = 1 << pin;
-
-    // Disable the interrupt first
-    GPIOx->IE &= ~pin_mask;
-
-    // Configure the interrupt type
-    switch (trigger) {
-        case GPIO_TRIGGER_LEVEL_LOW:
-            GPIOx->IS |= pin_mask;
-            GPIOx->IEV &= ~pin_mask;
-            break;
-        case GPIO_TRIGGER_LEVEL_HIGH:
-            GPIOx->IS |= pin_mask;
-            GPIOx->IEV |= pin_mask;
-            break;
-        case GPIO_TRIGGER_EDGE_FALLING:
-            GPIOx->IS &= ~pin_mask;
-            GPIOx->IBE &= ~pin_mask;
-            GPIOx->IEV &= ~pin_mask;
-            break;
-        case GPIO_TRIGGER_EDGE_RISING:
-            GPIOx->IS &= ~pin_mask;
-            GPIOx->IBE &= ~pin_mask;
-            GPIOx->IEV |= pin_mask;
-            break;
-        case GPIO_TRIGGER_EDGE_BOTH:
-            GPIOx->IS &= ~pin_mask;
-            GPIOx->IBE |= pin_mask;
-            break;
+void GPIO_EnableInterrupt(GPIO_TypeDef *GPIOx, uint8_t pin, bool is_edge, bool is_high) {
+    uint32_t pin_mask = GPIO_PIN_MASK(pin);
+    GPIOx->INTENSET = pin_mask;
+    if (is_edge) {
+        GPIOx->INTTYPESET = pin_mask;
+    } else {
+        GPIOx->INTTYPECLR = pin_mask;
     }
-
-    // Clear any previous interrupt
-    GPIOx->IC = pin_mask;
-
-    // Enable the interrupt
-    GPIOx->IE |= pin_mask;
+    if (is_high) {
+        GPIOx->INTPOLSET = pin_mask;
+    } else {
+        GPIOx->INTPOLCLR = pin_mask;
+    }
 }
 
 void GPIO_DisableInterrupt(GPIO_TypeDef *GPIOx, uint8_t pin) {
-    GPIOx->IE &= ~(1 << pin);
+    GPIOx->INTENCLR = GPIO_PIN_MASK(pin);
 }
 
 bool GPIO_GetInterruptStatus(GPIO_TypeDef *GPIOx, uint8_t pin) {
-    return (GPIOx->RIS & (1 << pin)) != 0;
+    // Note: This function assumes that there's an INTSTATUS register.
+    // If not, you may need to modify this based on how interrupt status is checked in your specific GPIO controller.
+    return (GPIOx->INTSTATUS_CLEAR & GPIO_PIN_MASK(pin)) != 0;
 }
 
 void GPIO_ClearInterrupt(GPIO_TypeDef *GPIOx, uint8_t pin) {
-    GPIOx->IC = (1 << pin);
+    GPIOx->INTSTATUS_CLEAR = GPIO_PIN_MASK(pin);
 }
